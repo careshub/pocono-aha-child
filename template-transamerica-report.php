7cf9cce9716473482ccd6a24644cbc39efeae90d
Amy Keith <amyakeith@gmail.com>
2023-07-27 17:07:41 -0500

    Replace Transamerica report logo and text with Edward Jones

--- template-transamerica-report.php
+++ template-transamerica-report.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Transamerica Report Page
+ * Template Name: Financial Advisors Report Page
  * Template Post Type: post, page
  *
  * Description: A custom template for displaying a fullwidth layout with no sidebar.
@@ -29,18 +29,17 @@ get_header(); ?>
 
 					<div class="post-content clearfix">
 
-						<div class="aha-screen-only" style="overflow:auto;">
+						<div class="aha-screen-only" style="overflow:auto; display: flex; align-items: end; justify-content: space-between; flex-wrap: wrap;">
 						    <img src="/wp-content/plugins/cc-aha-extras/includes/im/ta-report/aha-logo.jpg" style="margin-left: 2em; width: 150px;" alt="American Heart Association organizational logo">
-							<div class="ta-report-ta-logo" style="width: 200px; float:right; display:inline; margin-right: 2em;">
-						  		<img src="/wp-content/plugins/cc-aha-extras/includes/im/ta-report/transamerica-logo.jpg" style="" alt="Transamerica company logo">
-						  		<p style="text-align:left;margin-top:1em;margin-bottom:0;line-height:1.2;font-size:.6rem;">Transamerica is proud to be a national supporter of the American Heart Association</p>
+							<div class="ta-report-ta-logo" style="width: 200px; float:right; display:inline; margin-right: 2em; margin-left:2em">
+						  		<img src="/wp-content/plugins/cc-aha-extras/includes/im/ta-report/ej-logo.PNG" style="" alt="Edward Jones company logo">
 						  	</div>
 						</div>
 
 						<header class="entry-header aha-screen-only" style="text-align:center;margin-bottom:1.2em;">
 
 							<?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
-							<p class="aha-screen-only" style="margin:0 auto 0;max-width: 920px">Transamerica&reg; is proud to support the American Heart Association&rsquo;s Healthy for Good&trade; Movement. Through this collaboration, Transamerica and the American Heart Association (AHA) empower and equip Americans to live better today and worry less about tomorrow. Together, we&rsquo;ll guide people to make the most of their Wealth + Health&#8480; for a more fulfilling future.</p>
+							<p class="aha-screen-only" style="margin:0 auto 0;max-width: 920px">Edward Jones is proud to be a national supporter of the American Heart Association (AHA). Through this collaboration, Edward Jones and the AHA empower and equip all Americans &mdash; clients, colleagues, and community members &mdash; with tools and resources to promote optimal health and financial security.</p>
 
 						</header><!-- .entry-header -->
 
